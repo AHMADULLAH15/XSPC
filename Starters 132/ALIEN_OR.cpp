04aba39b13c0ec8fe0831b615a657804fb9a2447
AHMAD <140228240+AHMADULLAH15@users.noreply.github.com>
2024-06-13 23:50:07 +0600

    Add files via upload
    Added starters 132

---
+++ Starters 132/ALIEN_OR.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long
+#define loop(i,s,n) for(int i = s; i < n; i++)
+using namespace std;
+const int maxN = 1e2 + 9;
+int main()
+{
+    //Author : AHMAD
+    //Never Give UP AHMAD
+    ios ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> bit(maxN);
+        for (int i = 1;i <= n;i++) 
+        {
+            string s;
+            cin >> s;
+            int pos = 0, cnt = count(s.begin(), s.end(), '1');
+            if (cnt == 1) 
+            {
+                for (int j = k - 1;j >= 0;j--) 
+                {
+                    if (s[j] == '1') 
+                    {
+                        bit[pos] = 1;
+                    }
+                    pos++;
+                }
+            }
+        }
+
+        bool ok = true;
+        for (int i = 0;i < k;i++) 
+        {
+            if (bit[i] == 0) 
+            {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok) cout << "YES" << '\n';
+        else cout << "NO" << '\n';
+        
+    }
+
+    return 0;
+}
