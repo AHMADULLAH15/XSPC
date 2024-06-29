a287b7e03faf6dc4bdf81052f94b79278af66c87
AHMAD <140228240+AHMADULLAH15@users.noreply.github.com>
2024-06-29 21:46:24 +0600

    Add files via upload
    Added code day 5

---
+++ week 9/Day 5/G_Coin_Piles.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+#define loop(i,s,n) for(int i = s; i < n; i++)
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using pbds_mul = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main()
+{
+    //Author : AHMAD
+    //Never Give UP AHMAD
+    ios ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b;cin>>a>>b;
+
+        if(a > b) swap(a,b);
+        if(2 * a >= b && (a+b) % 3 == 0) cout<<"YES";
+        else cout<<"NO";
+        cout<<endl;
+
+    }
+
+    return 0;
+}
