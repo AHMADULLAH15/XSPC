bfbc7909f7de848bf0ab01cae4fb83cb7517735d
AHMAD <140228240+AHMADULLAH15@users.noreply.github.com>
2024-07-10 23:38:37 +0600

    Add files via upload
    Added day 2

---
+++ week 14/Day 2/B_Trick_Or_Treat.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+#define loop(i,s,n) for(int i = s; i < n; i++)
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using pbds_mul = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main()
+{
+    //Author : AHMAD
+    //Never Give UP AHMAD
+    ios ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;cin>>t;
+    while(t--)
+    {
+        int n,m,cnt = 0;cin>>n>>m;
+        vector<int> a(n),b(n);
+
+        map<int,int>mp;
+
+        loop(i,0,n)
+        {
+            cin>>a[i];
+            mp[a[i] % m]++;
+        }
+
+        loop(i,0,n)
+        {
+            cin>>b[i];
+        }
+
+        loop(i,0,n)
+        {
+            int y = b[i] % m;
+            int x = (m-y) % m;
+
+            cnt += mp[x];
+
+        }
+
+        cout<<cnt<<endl;
+    }
+
+    return 0;
+}
