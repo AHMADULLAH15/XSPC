646e7002fb78a4df4c516a2eec2e40e911776bd1
AHMAD <140228240+AHMADULLAH15@users.noreply.github.com>
2024-08-08 20:10:45 +0600

    Add files via upload
    Added day 2

---
+++ week16/Day 2/A_Target_Practice.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+const int mod = 1e9+7;
+#define loop(i,s,n) for(int i = s; i < n; i++)
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using pbds_mul = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main()
+{
+    //Author : AHMAD
+    //Never Give UP AHMAD
+    ios ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;cin>>t;
+    while(t--)
+    {
+        int ans = 0;
+
+        loop(i,0,10)
+        {
+            loop(j,0,10)
+            {
+                char x;cin>>x;
+
+                if(x == 'X')
+                {
+                    int val = min(i+1, 10-i);
+                    val = min(val , min((j+1) , 10-j));
+                    ans += val;
+                }
+            }
+        }
+
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
